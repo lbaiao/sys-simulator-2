6be1d43fbe75e58016a2d41fb2c10d8c497e6019
lucasbaiao <lucasbaiao@gmail.com>
2020-05-17 13:07:58 -0300

    new scratches

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 export LUCAS_PATH=$PWD
 
 # python scripts/script28.py
-python scripts/script29.py
-python scripts/script30.py
-python scratch/scratch25.py
-python scratch/scratch26.py
-python scratch/scratch27.py
+# python scripts/script29.py
+# python scripts/script30.py
+# python scratch/scratch25.py
+# python scratch/scratch26.py
+# python scratch/scratch27.py
+python scratch/scratch28.py
+python scratch/scratch29.py
+python scratch/scratch30.py

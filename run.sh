b9dc4b15b91199cad78b06f05b7e52d1a65a1e6b
lucasbaiao <lucasbaiao@gmail.com>
2020-05-17 11:22:43 -0300

    fix

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 export LUCAS_PATH=$PWD
 
-python3 scripts/script28.py
-python3 scripts/script29.py
-python3 scripts/script30.py
-python3 scratch/scratch25.py
-python3 scratch/scratch26.py
-python3 scratch/scratch27.py
+python scripts/script28.py
+python scripts/script29.py
+python scripts/script30.py
+python scratch/scratch25.py
+python scratch/scratch26.py
+python scratch/scratch27.py

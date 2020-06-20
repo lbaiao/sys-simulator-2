ebadfa68b8c31883d224e30a23dde09e6a302e58
lbaiao <lucasbaiao@gmail.com>
2020-06-20 16:50:08 -0300

    rewards logging, model saving

--- scripts_a2c/script1.py
+++ scripts_a2c/script1.py
@@ -57,11 +57,11 @@ user_gain = gen.db_to_power(user_gain)
 sinr_threshold_train = gen.db_to_power(sinr_threshold_train)
 
 # q-learning parameters
-STEPS_PER_EPISODE = 5
+STEPS_PER_EPISODE = 10
 EPSILON_MIN = 0.05
 EPSILON_DECAY = 0.8375*1e-4    # long training
 # EPSILON_DECAY = 3.35*1e-4    # medium training
-MAX_NUM_EPISODES = 2000      # long training
+MAX_NUM_EPISODES = 20000      # long training
 # MAX_NUM_EPISODES = 480      # medium training
 # MAX_NUM_EPISODES = 2      # quick test
 ALPHA = 0.2  # Learning rate
@@ -94,6 +94,7 @@ optimizer = optim.Adam(a2c.parameters(), lr=LEARNING_RATE)
 episode = 0
 mue_spectral_eff_bag = list()
 d2d_spectral_eff_bag = list()
+mean_rewards = []
 while episode < MAX_NUM_EPISODES:
     log_probs = []
     values = []
@@ -152,104 +153,22 @@ while episode < MAX_NUM_EPISODES:
     optimizer.step()
     episode += 1
 
+    m_reward = torch.mean(torch.cat(rewards)).item()
+    mean_rewards.append(m_reward)
     print("Episode#:{} mean reward:{}".format(
-        episode, torch.mean(torch.cat(rewards)).item()))
+        episode, m_reward))
 
-# # training function
-# # TODO: colocar agente e d2d_device na mesma classe? fazer propriedade d2d_device no agente?
-# def train(framework: ExternalDQNFramework, env: CompleteEnvironment2, params: TrainingParameters, agent_params: DQNAgentParameters, max_d2d: int):
-#     best_reward = float('-inf')
-#     device = torch.device('cuda')
-#     mue_spectral_eff_bag = list()
-#     d2d_spectral_eff_bag = list()
-#     aux_range = range(max_d2d+1)[1:]
-#     epsilon = agent_params.start_epsilon
-#     for episode in range(params.max_episodes):
-#         # TODO: atualmente redistribuo os usuarios aleatoriamente a cada episodio. Isto é o melhor há se fazer? 
-#         # Simular deslocamento dos usuários?
-#         actions = [i*0.82*p_max/5/1000 for i in range(5)] # best result
-#         n_agents = np.random.choice(aux_range)
-#         agents = [ExternalDQNAgent(agent_params, actions) for i in range(n_agents)] # 1 agent per d2d tx
-#         counts = np.zeros(len(agents))
-#         awaits = list()
-#         await_steps = [2,3,4]
-#         for a in agents:
-#             awaits.append(np.random.choice(await_steps))
-#             a.set_action(torch.tensor(0).long().cuda(), a.actions[0])
-#             a.set_epsilon(epsilon)
+cwd = os.getcwd()
 
-#         env.build_scenario(agents)
-#         done = False
-#         obs = [env.get_state(a) for a in agents] 
-#         total_reward = 0.0
-#         i = 0
-#         bag = list()
-#         while not done:
-#             if i >= params.steps_per_episode:
-#                 break
-#             else:
-#                 actions = torch.zeros([len(agents)], device=device)
-#                 for j, agent in enumerate(agents):
-#                     if counts[j] < awaits[j]:
-#                         counts[j] += 1
-#                     else:
-#                         agent.get_action(framework, obs[j])
-#                         actions[j] = agent.action_index
-#                         counts[j] = 0
-#                         awaits[j] = np.random.choice(await_steps)
-#                 next_obs, rewards, done = env.step(agents)
-#                 i += 1
-#                 for j, agent in enumerate(agents):
-#                     framework.replay_memory.push(obs[j], actions[j], next_obs[j], rewards[j])
-#                 framework.learn()
-#                 obs = next_obs
-#                 total_reward += torch.sum(rewards)
-#                 bag.append(total_reward.item())   
-#                 obs = next_obs
-#                 if episode % TARGET_UPDATE == 0:
-#                     framework.target_net.load_state_dict(framework.policy_net.state_dict())
-#             if total_reward > best_reward:
-#                 best_reward = total_reward
-#             print("Episode#:{} sum reward:{} best_sum_reward:{} eps:{}".format(episode,
-#                                      total_reward, best_reward, agents[0].epsilon))
+data = {}
+data['mean_rewards'] = mean_rewards
 
-#             # some statistics
-#             mue_spectral_eff_bag.append(env.mue_spectral_eff)     # mue spectral eff
-#             d2d_spectral_eff_bag.append(env.d2d_spectral_eff/env.params.n_d2d)   # average d2d spectral eff
-#         epsilon = agents[0].epsilon
-
-    
-#     # Return the trained policy
-#     return mue_spectral_eff_bag, d2d_spectral_eff_bag
-
-            
-# # SCRIPT EXEC
-# # training
-# mue_spectral_effs, d2d_spectral_effs = train(ext_framework, environment, train_params, agent_params, MAX_NUMBER_OF_AGENTS)
-# spectral_effs = zip(mue_spectral_effs, d2d_spectral_effs)
-
-# cwd = os.getcwd()
-
-# filename = gen.path_leaf(__file__)
-# filename = filename.split('.')[0]
-# filename_model = filename
-# filename = f'{lucas_path}/data/{filename}.pickle'
-# torch.save(ext_framework.policy_net.state_dict(), f'{lucas_path}/models/{filename_model}.pt')
-# with open(filename, 'wb') as f:
-#     pickle.dump(spectral_effs, f)
-
-# # plt.figure(1)
-# # plt.plot(mue_spectral_effs, '.', label='MUEs')
-# # plt.plot(d2d_spectral_effs, '.', label='D2Ds')
-# # plt.xlabel('Iteration')
-# # plt.ylabel('Average Spectral Efficiencies')
-# # plt.legend()
-
-# plt.figure(1)
-# plt.plot(ext_framework.bag, '.')
-# plt.xlabel('Iteration')
-# plt.ylabel('Average Q-Values')
-
-# plt.show()
-
-
+filename = gen.path_leaf(__file__)
+filename = filename.split('.')[0]
+filename_model = filename
+filename = f'{lucas_path}/data/{filename}.pickle'
+torch.save(
+    a2c.state_dict(),
+    f'{lucas_path}/models/{filename_model}.pt')
+with open(filename, 'wb') as f:
+    pickle.dump(data, f)

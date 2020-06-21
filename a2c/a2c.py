5b258f140fc9a962625029ee0c060261c58dfdfd
lucasbaiao <lucasbaiao@gmail.com>
2020-06-21 12:57:28 -0300

    NNs update

--- a2c/a2c.py
+++ a2c/a2c.py
@@ -23,13 +23,17 @@ class ActorCritic(nn.Module):
 
         self.critic = nn.Sequential(
             nn.Linear(num_inputs, hidden_size),
-            nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.Tanh(),
             nn.Linear(hidden_size, 1)
         ).to(self.device)
 
         self.actor = nn.Sequential(
             nn.Linear(num_inputs, hidden_size),
-            nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.Tanh(),
             nn.Linear(hidden_size, num_outputs),
             nn.Softmax(dim=1),
         ).to(self.device)

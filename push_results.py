1c692ed229c55e137a5e04fd44c2270c22dffbd1
lbaiao <lucasbaiao@gmail.com>
2020-07-05 15:45:37 -0300

    automated results git push

--- push_results.py
+++ push_results.py
@@ -1,11 +1,6 @@
-from scripts_a2c.script1 import run
-from scratch_a2c.scratch1 import run as run_test
 import os
 
-# run training and tests
-run()
-run_test()
-# commit new results
+
 os.system('git add data/*')
 os.system('git add data/a2c/*')
 os.system('git add models/*')
@@ -14,5 +9,3 @@ os.system('git add logs/*')
 os.system('git add logs/a2c/*')
 os.system('git commit -m "new results"')
 os.system('git push origin devel')
-
-print('DONE')

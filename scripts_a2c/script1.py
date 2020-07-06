de9f01672f197a456ea7df1dda3077196139d3c2
lbaiao <lucasbaiao@gmail.com>
2020-07-06 15:41:05 -0300

    lr update

--- scripts_a2c/script1.py
+++ scripts_a2c/script1.py
@@ -46,7 +46,7 @@ def run():
     C = 80  # C constant for the improved reward function
     MAX_NUMBER_OF_AGENTS = 10
     HIDDEN_SIZE = 256
-    LEARNING_RATE = 9
+    LEARNING_RATE = .9999
     # mu = 0.82*p_max/5/2000
     # std = mu/6
     mu = 0

9def42201ee16d8c279d7a437cc26839da20c4f8
lbaiao <lucasbaiao@gmail.com>
2020-07-11 13:26:48 -0300

    automation script is now better organized

---
+++ exec.py
@@ -0,0 +1,7 @@
+from scripts_a2c.script1 import run_training
+from scratch_a2c.scratch1 import run as run_test
+
+
+# run training and tests
+run_training()
+run_test()
--- run.py
+++ run.py
@@ -2,12 +2,7 @@ import os
 
 # pulls the code most recent version
 os.system('git pull origin devel')
-# import up-to-date dependencies
-from scripts_a2c.script1 import run
-from scratch_a2c.scratch1 import run as run_test
-# run training and tests
-run()
-run_test()
+os.system('python exec.py')
 # commit new results
 os.system('git add data/*')
 os.system('git add data/a2c/*')

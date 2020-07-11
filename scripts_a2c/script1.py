bug fix. The sum was done on the wrong axis.

scripts_a2c/script1.py
@@ -114,7 +114,7 @@ def run():
         critic_optimizer.step()
         # update actor
         aux = torch.mul(advantages, log_probs)
-        aux = torch.sum(aux, axis=0)
+        aux = torch.sum(aux, axis=1)
         actor_loss = -torch.mean(aux)
         actor_optimizer.zero_grad()
         actor_loss.backward()
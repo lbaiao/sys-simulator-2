e5300fa34a9fa77d836cf40c04cf2bb81f0f7eee
lbaiao <lucasbaiao@gmail.com>
2020-07-14 21:44:55 -0300

    added a2c discrete. commented out init_weights

--- sys_simulator/a2c/a2c.py
+++ sys_simulator/a2c/a2c.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.distributions import Normal
+from torch.distributions import Normal, Categorical
 
 
 def compute_gae_returns(device, rewards: torch.Tensor,
@@ -59,16 +59,45 @@ class ActorCritic(nn.Module):
 
         self.log_std = nn.Parameter(torch.ones(1, num_outputs) * std)
 
-        def init_weights(m):
-            if isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, mean=self.mean, std=self.std)
-                nn.init.constant_(m.bias, 0.0)
+        # def init_weights(m):
+        #     if isinstance(m, nn.Linear):
+        #         nn.init.normal_(m.weight, mean=self.mean, std=self.std)
+        #         nn.init.constant_(m.bias, 0.0)
 
-        self.apply(init_weights)
+        # self.apply(init_weights)
 
     def forward(self, x):
         value = self.critic(x).to(self.device)
         mu = self.actor(x.view(1, -1))
         std = self.log_std.exp().expand_as(mu).to(self.device)
         dist = Normal(mu, std)
+        return dist, value
+
+
+class ActorCriticDiscrete(nn.Module):
+    def __init__(self, num_inputs, num_outputs,
+                 hidden_size, mean=0.0, std=0.1):
+        super(ActorCritic, self).__init__()
+        self.mean = mean
+        self.std = std
+        self.device =\
+            torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        ).to(self.device)
+
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+            nn.Softmax(dim=1),
+        ).to(self.device)
+
+    def forward(self, x):
+        value = self.critic(x).to(self.device)
+        probs = self.actor(x.view(1, -1))
+        dist = Categorical(probs)
         return dist, value

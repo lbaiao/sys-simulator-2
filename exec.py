2377be5c12916a9ff46187e4dce5d74de7a7ff55
lbaiao <lucasbaiao@gmail.com>
2020-07-27 19:12:08 -0300

    script6

--- exec.py
+++ exec.py
@@ -1,5 +1,5 @@
-from scripts_a2c.script5 import run as run_training
-from scratch_a2c.scratch5 import run as run_test
+from scripts_a2c.script6 import run as run_training
+from scratch_a2c.scratch6 import run as run_test
 
 
 # run training and tests

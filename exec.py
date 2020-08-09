ca32c3f520db597a8959ffb9bb1e77da66ac9bc2
lbaiao <lucasbaiao@gmail.com>
2020-08-09 12:09:02 -0300

    10 test

--- exec.py
+++ exec.py
@@ -1,7 +1,7 @@
 from scripts_a2c.script2 import run as run_training
-from scratch_a2c.scratch2 import run as run_test
+from scratch_a2c.scratch10 import run as run_test
 
 
 # run training and tests
-run_training()
+# run_training()
 run_test()

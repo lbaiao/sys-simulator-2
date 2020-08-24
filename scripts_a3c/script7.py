853be019e0d586bc89d9c812e76007b3bf68625a
lbaiao <lucasbaiao@gmail.com>
2020-08-24 12:24:44 -0300

    changed actions

--- scripts_a3c/script7.py
+++ scripts_a3c/script7.py
@@ -80,7 +80,7 @@ def run():
     episode = 0
     d2d_spectral_effs = []
     mue_spectral_effs = []
-    actions = np.linspace(1e-4, 1e-3, 5)[::-1] * p_max
+    actions = np.linspace(1e-5, 1e-4, 5)[::-1] * p_max
     values_total = []
     log_probs_total = []
     rewards_total = []

93ced68dbf3ec8011a328bc37cb8c5e199c13b1e
lbaiao <lucasbaiao@gmail.com>
2020-09-13 14:42:49 -0300

    db reward

--- sys_simulator/q_learning/rewards.py
+++ sys_simulator/q_learning/rewards.py
@@ -1,10 +1,14 @@
+from sys_simulator.general.general import db_to_power
 import numpy as np
 import torch
 
 from typing import List
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def centralized_reward(sinr_mue: float, sinr_d2ds: List[float], *args, **kwargs):
+
+def centralized_reward(sinr_mue: float, sinr_d2ds: List[float],
+                       *args, **kwargs):
     mue_contrib = np.log2(1 + sinr_mue)
     d2d_contrib = sum([np.log2(1 + s) for s in sinr_d2ds])
     reward = mue_contrib + d2d_contrib
@@ -70,6 +74,20 @@ def dis_reward_tensor2(sinr_mue: float, sinr_d2ds: List[float],
     sinr_d2ds = torch.tensor(sinr_d2ds, device=device)
     d2d_contrib = torch.sum(torch.log2(1 + sinr_d2ds))
     rewards = -10/C * torch.ones(len(sinr_d2ds))
+    if state:
+        for i in range(len(sinr_d2ds)):
+            rewards[i] = 1/C * torch.log2(1 + sinr_d2ds[i])
+    return rewards, mue_contrib, d2d_contrib
+
+
+def dis_reward_tensor_db(sinr_mue_db: float, sinr_d2ds_db: List[float],
+                         state: bool, C: float, *args, **kwargs):
+    sinr_mue = db_to_power(sinr_mue_db)
+    sinr_d2ds = db_to_power(sinr_d2ds_db)
+    mue_contrib = torch.log2(1 + torch.tensor(sinr_mue, device=device))
+    sinr_d2ds = torch.tensor(sinr_d2ds, device=device)
+    d2d_contrib = torch.sum(torch.log2(1 + sinr_d2ds))
+    rewards = -1 * torch.ones(len(sinr_d2ds))
     if state:
         for i in range(len(sinr_d2ds)):
             rewards[i] = 1/C * torch.log2(1 + sinr_d2ds[i])

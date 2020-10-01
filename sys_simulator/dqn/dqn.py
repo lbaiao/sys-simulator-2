9c8d8e4d42ceb7ffbb9f09292ffe4230dae269b0
lbaiao <lucasbaiao@gmail.com>
2020-10-01 19:19:11 -0300

    refactoring

--- sys_simulator/dqn/dqn.py
+++ sys_simulator/dqn/dqn.py
@@ -209,10 +209,10 @@ class DQN(torch.nn.Module):
 
     def forward(self, obs):
         x = self.fc1(obs)
-        x = F.tanh(x)
+        x = torch.tanh(x)
         for i in self.hidden_layers:
             x = i(x)
-            x = F.tanh(x)
+            x = torch.tanh(x)
         x = self.fc2(x)
         output = F.softmax(x, dim=1)
         return output

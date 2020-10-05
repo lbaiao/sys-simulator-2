b8a51be0bcdf27f109229e0dc0cb699732c70b9c
lbaiao <lucasbaiao@gmail.com>
2020-10-05 10:57:58 -0300

    new powers and num_episodes

--- scripts_dql/script32.py
+++ scripts_dql/script32.py
@@ -36,17 +36,17 @@ CHANNEL_RND = False
 # training
 STEPS_PER_EPISODE = 10
 # MAX_NUM_EPISODES = 110      # fast training
-MAX_NUM_EPISODES = 550 * 5      # fast training
+MAX_NUM_EPISODES = 550 * 3      # fast training
 # MAX_NUM_EPISODES = 10      # debugging
 # testing
 # TEST_NUM_EPISODES = 110
-TEST_NUM_EPISODES = 550 * 5
+TEST_NUM_EPISODES = 550 * 3
 # TEST_NUM_EPISODES = 1  # testing
 TEST_STEPS_PER_EPISODE = 10
 # common
 EPSILON_MIN = 0.05
 # EPSILON_DECAY = 1e-3    # fast training
-EPSILON_DECAY = .2e-3 / 4    # fast training
+EPSILON_DECAY = .2e-3 / 2    # fast training
 # EPSILON_DECAY = .4167*1e-3    # long training
 # EPSILON_DECAY = .04167*1e-3    # super long training
 # GAMMA = 0.98  # Discount factor
@@ -65,7 +65,7 @@ max_d2d = MAX_NUMBER_OF_AGENTS
 #     db_to_power(p_max-20), db_to_power(p_max-10), 10
 # ))
 # db discretization
-actions = np.linspace(p_max-28, p_max-18, 10)
+actions = np.linspace(p_max-20, p_max-16, 10)
 actions[0] = -1000
 env_params = EnvironmentParameters(
     rb_bandwidth, d2d_pair_distance, p_max, noise_power,

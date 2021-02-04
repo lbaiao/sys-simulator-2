1a2186457736e1655d66d51f9d84453063a3f314
lbaiao <lucasbaiao@gmail.com>
2021-02-04 08:29:13 -0300

    exec

--- exec.py
+++ exec.py
@@ -1,5 +1,5 @@
-# from scripts_dql.script46 import run
-from scripts_a2c.script10 import run
+from scripts_dql.script49 import run
+# from scripts_a2c.script10 import run
 
 
 # run training and tests

bbfb8c7066ee404f1008209fd84f554f6d772867
lbaiao <lucasbaiao@gmail.com>
2021-02-05 10:17:10 -0300

    benchmark script run

--- exec.py
+++ exec.py
@@ -1,5 +1,6 @@
-from scripts_dql.script48 import run
+# from scripts_dql.script48 import run
 # from scripts_a2c.script10 import run
+from scripts_benchmarks.script1 import run
 
 
 # run training and tests

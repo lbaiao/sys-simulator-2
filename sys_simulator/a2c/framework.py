1b5be81f400a0adedf02b43587bc2d87743c2844
lbaiao <lucasbaiao@gmail.com>
2021-02-12 09:29:50 -0300

    enabled multiple environments

--- sys_simulator/a2c/framework.py
+++ sys_simulator/a2c/framework.py
@@ -9,12 +9,14 @@ class Framework:
     values: torch.Tensor
     rewards: torch.Tensor
     entropy: torch.Tensor
+    n_environments: int
     steps_per_episode: int
-    beta: int
+    beta: float
     device: torch.device
 
     def __init__(
         self,
+        n_environments: int,
         steps_per_episode: int,
         beta: float,
         gamma: float,
@@ -25,12 +27,35 @@ class Framework:
         self.device = device
         self.beta = beta
         self.gamma = gamma
+        self.n_environments = n_environments
         self.steps_per_episode = steps_per_episode
         self.criterion = torch.nn.MSELoss()
-        self.log_probs = torch.zeros((1, steps_per_episode)).to(device)
-        self.values = torch.zeros((1, steps_per_episode+1)).to(device)
-        self.rewards = torch.zeros((1, steps_per_episode)).to(device)
-        self.entropy = torch.zeros((1, steps_per_episode)).to(device)
+        self.log_probs = \
+            torch.zeros((self.n_environments, self.steps_per_episode))\
+            .float().to(self.device)
+        self.values = \
+            torch.zeros((self.n_environments, self.steps_per_episode+1))\
+            .float().to(self.device)
+        self.rewards = \
+            torch.zeros((self.n_environments, self.steps_per_episode))\
+            .float().to(self.device)
+        self.entropy = \
+            torch.zeros((self.n_environments, self.steps_per_episode))\
+            .float().to(self.device)
+
+    def reset_values(self, n_environments):
+        self.log_probs = \
+            torch.zeros((n_environments, self.steps_per_episode))\
+            .float().to(self.device)
+        self.values = \
+            torch.zeros((n_environments, self.steps_per_episode+1))\
+            .float().to(self.device)
+        self.rewards = \
+            torch.zeros((n_environments, self.steps_per_episode))\
+            .float().to(self.device)
+        self.entropy = \
+            torch.zeros((n_environments, self.steps_per_episode))\
+            .float().to(self.device)
 
     def learn(self):
         # gae and returns
@@ -39,27 +64,24 @@ class Framework:
                 self.device, self.rewards, self.values, self.gamma
             )
         # update critic
-        critic_loss = self.criterion(self.values[0][:-1], returns)
+        critic_loss = self.criterion(
+            self.values[:, :-1].reshape(1, -1),
+            returns.view(1, -1)
+        )
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
         # update actor
         actor_loss = torch.mul(advantages, self.log_probs)
         actor_loss -= self.beta * self.entropy
-        actor_loss = -torch.sum(actor_loss)
+        actor_loss = torch.sum(actor_loss, axis=1)
+        actor_loss = -torch.mean(actor_loss)
         # actor_loss = -torch.mean(actor_loss)
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
         # reset values
-        self.log_probs = \
-            torch.zeros((1, self.steps_per_episode)).float().to(self.device)
-        self.values = \
-            torch.zeros((1, self.steps_per_episode+1)).float().to(self.device)
-        self.rewards = \
-            torch.zeros((1, self.steps_per_episode)).float().to(self.device)
-        self.entropy = \
-            torch.zeros((1, self.steps_per_episode)).float().to(self.device)
+        self.reset_values(self.n_environments)
 
 
 class DiscreteFramework(Framework):
@@ -74,6 +96,7 @@ class DiscreteFramework(Framework):
         output_size: int,
         hidden_size: int,
         n_hidden_layers: int,
+        num_environment: int,
         steps_per_episode: int,
         learning_rate: float,
         beta: float,
@@ -83,7 +106,7 @@ class DiscreteFramework(Framework):
         )
     ):
         super(DiscreteFramework, self).__init__(
-            steps_per_episode, beta, gamma, device
+            num_environment, steps_per_episode, beta, gamma, device
         )
         self.a2c = ActorCriticDiscrete(
             input_size,
@@ -121,7 +144,7 @@ class ContinuousFramework(Framework):
         )
     ):
         super(ContinuousFramework, self).__init__(
-            steps_per_episode, beta, gamma, device
+            1, steps_per_episode, beta, gamma, device
         )
         self.mu = torch.zeros((1, steps_per_episode)).to(device)
         self.actions = torch.zeros((1, steps_per_episode)).to(device)

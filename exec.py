046d089b59b6b43cede5e8a9e76fc886d1a7c5b2
lbaiao <lucasbaiao@gmail.com>
2021-02-13 14:10:50 -0300

    params

--- exec.py
+++ exec.py
@@ -1,5 +1,5 @@
-# from scripts_dql.script48 import run
-from scripts_a2c.script15 import run
+from scripts_dql.script47 import run
+# from scripts_a2c.script15 import run
 # from scripts_benchmarks.script2 import run
 
 

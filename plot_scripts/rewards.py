eec65d4335f713736eeabec20838b31bd99a624a
lbaiao <lucasbaiao@gmail.com>
2021-04-17 21:06:29 -0300

    hunks 1

--- plot_scripts/rewards.py
+++ plot_scripts/rewards.py
@@ -7,14 +7,19 @@ import matplotlib.pyplot as plt
 # sinr linear
 min_mue_sinr = db_to_power(6)
 n_samples = 1000
-d2d_sinrs = np.linspace(0, 5, n_samples)
-mue_sinr1 = np.linspace(0, min_mue_sinr, n_samples)
-mue_sinr2 = np.linspace(min_mue_sinr, 5, n_samples)
+mue_sinr1 = np.linspace(-10, min_mue_sinr, n_samples)
+mue_sinr2 = np.linspace(min_mue_sinr, 10, n_samples)
+mue_sinr3 = np.linspace(10, 20, n_samples)
+d2d_sinrs1 = np.linspace(-10, 10, n_samples)
+d2d_sinrs2 = np.linspace(-10, 10, n_samples)
+d2d_sinrs3 = np.linspace(10, 20, n_samples)
 
 
 def reward(mue_sinr, d2d_sinrs, min_mue_sinr):
     if mue_sinr > min_mue_sinr:
-        reward = d2d_sinrs
+        reward = np.sum(db_to_power(d2d_sinrs))
+        reward = 1e-9 if reward == 0 else reward
+        reward = 0.2 * power_to_db(reward)
     else:
         reward = (mue_sinr - min_mue_sinr)**3
     return reward
